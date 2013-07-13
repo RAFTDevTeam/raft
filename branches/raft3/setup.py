39fb87cef8b212ea11c6af05a3306f0a59f324d8
gfleischer.fns@gmail.com <gfleischer.fns@gmail.com@1650b48b-b3f2-e7c8-9b9e-02247df4ad69>
2013-07-13 16:57:28 +0000

    Modifed setup.py to use cx_freeze

--- branches/raft3/setup.py
+++ branches/raft3/setup.py
@@ -2,7 +2,7 @@
 #
 # Author: Gregory Fleischer (gfleischer@gmail.com)
 #
-# Copyright (c) 2011 RAFT Team
+# Copyright (c) 2011-2013 RAFT Team
 #
 # This file is part of RAFT.
 #
@@ -20,87 +20,139 @@
 # along with RAFT.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from distutils.core import setup
 import sys
 import os
-import glob
+from cx_Freeze import setup, Executable
 
-def walk_datalist(obj, dirname, entries):
-    if '.svn' in dirname:
-        return
-    items = []
-    for entry in entries:
-        if '.svn' != entry:
-            filename = os.path.join(dirname, entry)
-            if os.path.isfile(filename):
-                items.append(filename)
-    obj[dirname] = items
+includes = ['lxml.etree', 'lxml._elementpath', 'lxml.html', 'gzip', 'sip', 'PyQt4.QtWebKit', 're']
 
-def walk_MSVCP90(obj, dirname, entries):
-    if obj['found']:
-        return
-    for entry in entries:
-        if entry.lower() == 'msvcp90.dll':
-            sys.path.append(dirname)
-            obj['found'] = True
-            return
+def gather_include_files(dirname):
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(dirname):
+        for filename in filenames:
+            if filename.endswith('.py'):
+                source = target = os.path.join(dirpath, filename)
+                files.append((source, target))
 
-def find_MSVCP90():
-    obj = {'found': False}
-    os.path.walk(os.environ.get('SystemRoot'), walk_MSVCP90, obj)
-    if not obj['found']:
-        os.path.walk(os.environ.get('ProgramFiles'), walk_MSVCP90, obj)
+    return files
 
-def get_datafiles():
-    datalist = {}
-    os.path.walk('thirdparty', walk_datalist, datalist)
-    os.path.walk('data', walk_datalist, datalist)
-    os.path.walk('analyzers', walk_datalist, datalist)
-    data_files = []
-    keys = list(datalist.keys())
-    keys.sort()
-    for key in keys:
-        if len(datalist[key]) > 0:
-            data_files.append((key, datalist[key]))
-    return data_files
+zip_includes = []
+zip_includes.extend(gather_include_files('thirdparty'))
 
-data_files = get_datafiles()
-data_files.append(('extras', [os.path.join('extras', f) for f in ('RaftCapture.dtd', 'RaftCaptureProcessor.py')]))
+include_files = []
+include_files.extend(gather_include_files('analyzers'))
+include_files.extend(('data', 'data'))
+for f in ('RaftCapture.dtd', 'RaftCaptureProcessor.py'):
+    include_files.append((os.path.join('extras', f), os.path.join('extras', f)))
 
-if 'darwin' == sys.platform:
-    import py2app
-    setup(
-        name = 'RAFT',
-        app = ['raft.pyw'],
-        data_files = data_files,
-          options = {
-            'py2app': 
-            {
-                'includes': ['lxml.etree', 'lxml._elementpath', 'gzip', 'sip'],
-                }
-            }
-        )
-    # need to customize qt.conf to avoid dup library loading
-    fh = open('dist/RAFT.app/Contents/Resources/qt.conf', 'wb')
-    fh.write('')
-    fh.close()
+base = None
+if 'win32' == sys.platform:
+    base = 'Win32GUI'
+    base = None # TODO: remove once log messages are correctly routed for Gui
 
-elif 'win32' == sys.platform:
-    import py2exe
+exe = Executable(
+    script = 'raft.py',
+    base = base,
+    targetName = 'raft.exe'
+    )
 
-    find_MSVCP90()
+# TODO: for MSI, need to add shortcut to program menu or desktop (http://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop)
 
-    setup(
-        name = 'RAFT',
-        console = ['raft.pyw'],
-          data_files = data_files,
-          options = {
-            'py2exe': 
-            {
-                'includes': ['lxml.etree', 'lxml._elementpath', 'gzip', 'sip'],
-                }
-            }
-          )
-else:
-    pass
+setup(
+    name = 'RAFT',
+    version = '3.0.1', # TODO: determine method to expose version between setup and raft proper
+    description = 'RAFT - Response Analysis and Further Testing',
+    options = {'build_exe': {
+            'includes' : includes,  
+            'include_files' : include_files, 
+            'zip_includes' : zip_includes, 
+            'include_msvcr' : True,
+            } },
+    executables = [exe]
+    )
+
+### TODO: remove this old reference code after all platforms are confirm working
+
+# from distutils.core import setup
+# import os
+# import glob
+
+# def walk_datalist(obj, dirname, entries):
+#     if '.svn' in dirname:
+#         return
+#     items = []
+#     for entry in entries:
+#         if '.svn' != entry:
+#             filename = os.path.join(dirname, entry)
+#             if os.path.isfile(filename):
+#                 items.append(filename)
+#     obj[dirname] = items
+
+# def walk_MSVCP90(obj, dirname, entries):
+#     if obj['found']:
+#         return
+#     for entry in entries:
+#         if entry.lower() == 'msvcp90.dll':
+#             sys.path.append(dirname)
+#             obj['found'] = True
+#             return
+
+# def find_MSVCP90():
+#     obj = {'found': False}
+#     os.path.walk(os.environ.get('SystemRoot'), walk_MSVCP90, obj)
+#     if not obj['found']:
+#         os.path.walk(os.environ.get('ProgramFiles'), walk_MSVCP90, obj)
+
+# def get_datafiles():
+#     datalist = {}
+#     os.path.walk('thirdparty', walk_datalist, datalist)
+#     os.path.walk('data', walk_datalist, datalist)
+#     os.path.walk('analyzers', walk_datalist, datalist)
+#     data_files = []
+#     keys = list(datalist.keys())
+#     keys.sort()
+#     for key in keys:
+#         if len(datalist[key]) > 0:
+#             data_files.append((key, datalist[key]))
+#     return data_files
+
+# data_files = get_datafiles()
+# data_files.append(('extras', [os.path.join('extras', f) for f in ('RaftCapture.dtd', 'RaftCaptureProcessor.py')]))
+
+# if 'darwin' == sys.platform:
+#     import py2app
+#     setup(
+#         name = 'RAFT',
+#         app = ['raft.pyw'],
+#         data_files = data_files,
+#           options = {
+#             'py2app': 
+#             {
+#                 'includes': ['lxml.etree', 'lxml._elementpath', 'gzip', 'sip'],
+#                 }
+#             }
+#         )
+#     # need to customize qt.conf to avoid dup library loading
+#     fh = open('dist/RAFT.app/Contents/Resources/qt.conf', 'wb')
+#     fh.write('')
+#     fh.close()
+
+# elif 'win32' == sys.platform:
+#     import py2exe
+
+#     find_MSVCP90()
+
+#     setup(
+#         name = 'RAFT',
+#         console = ['raft.pyw'],
+#           data_files = data_files,
+#           options = {
+#             'py2exe': 
+#             {
+#                 'includes': ['lxml.etree', 'lxml._elementpath', 'gzip', 'sip'],
+#                 }
+#             }
+#           )
+# else:
+#     pass
 
