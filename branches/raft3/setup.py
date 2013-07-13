c32d72b8b272b34b5444bc96589ae1848a6a817e
gfleischer <gfleischer@1650b48b-b3f2-e7c8-9b9e-02247df4ad69>
2013-07-13 20:05:05 +0000

    Added darwin target support

--- branches/raft3/setup.py
+++ branches/raft3/setup.py
@@ -24,7 +24,7 @@ import sys
 import os
 from cx_Freeze import setup, Executable
 
-includes = ['lxml.etree', 'lxml._elementpath', 'lxml.html', 'gzip', 'sip', 'PyQt4.QtWebKit', 're']
+includes = ['lxml.etree', 'lxml._elementpath', 'lxml.html', 'gzip', 'sip', 'PyQt4.QtWebKit', 'PyQt4.Qsci', 're']
 
 def gather_include_files(dirname):
     files = []
@@ -42,18 +42,35 @@ zip_includes.extend(gather_include_files('thirdparty'))
 include_files = []
 include_files.extend(gather_include_files('analyzers'))
 include_files.extend(('data', 'data'))
+include_files.extend(gather_include_files('thirdparty'))
 for f in ('RaftCapture.dtd', 'RaftCaptureProcessor.py'):
     include_files.append((os.path.join('extras', f), os.path.join('extras', f)))
 
+bin_excludes = []
+
 base = None
+include_msvcr = False
 if 'win32' == sys.platform:
     base = 'Win32GUI'
     base = None # TODO: remove once log messages are correctly routed for Gui
+    targetName = 'raft.exe'
+    include_msvcr = True
+elif 'darwin' == sys.platform:
+
+    # exclude: libQsci.dylib
+    # the compiled file is copied from libQsci.dylib to Qsci.so during build process
+    # this leaves the original library name intact and cx_Freeze cannot detect this
+    # so to get this to build cleanly, need to exclude libQsci.dylib file
+    bin_excludes.append('libQsci.dylib')
+
+    targetName = 'RAFT'
+else: # must be linux or other platform
+    targetName = 'raft.py'
 
 exe = Executable(
     script = 'raft.py',
     base = base,
-    targetName = 'raft.exe'
+    targetName = targetName
     )
 
 # TODO: for MSI, need to add shortcut to program menu or desktop (http://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop)
@@ -66,93 +83,8 @@ setup(
             'includes' : includes,  
             'include_files' : include_files, 
             'zip_includes' : zip_includes, 
-            'include_msvcr' : True,
+            'include_msvcr' : include_msvcr,
+            'bin_excludes' : bin_excludes,
             } },
     executables = [exe]
     )
-
-### TODO: remove this old reference code after all platforms are confirm working
-
-# from distutils.core import setup
-# import os
-# import glob
-
-# def walk_datalist(obj, dirname, entries):
-#     if '.svn' in dirname:
-#         return
-#     items = []
-#     for entry in entries:
-#         if '.svn' != entry:
-#             filename = os.path.join(dirname, entry)
-#             if os.path.isfile(filename):
-#                 items.append(filename)
-#     obj[dirname] = items
-
-# def walk_MSVCP90(obj, dirname, entries):
-#     if obj['found']:
-#         return
-#     for entry in entries:
-#         if entry.lower() == 'msvcp90.dll':
-#             sys.path.append(dirname)
-#             obj['found'] = True
-#             return
-
-# def find_MSVCP90():
-#     obj = {'found': False}
-#     os.path.walk(os.environ.get('SystemRoot'), walk_MSVCP90, obj)
-#     if not obj['found']:
-#         os.path.walk(os.environ.get('ProgramFiles'), walk_MSVCP90, obj)
-
-# def get_datafiles():
-#     datalist = {}
-#     os.path.walk('thirdparty', walk_datalist, datalist)
-#     os.path.walk('data', walk_datalist, datalist)
-#     os.path.walk('analyzers', walk_datalist, datalist)
-#     data_files = []
-#     keys = list(datalist.keys())
-#     keys.sort()
-#     for key in keys:
-#         if len(datalist[key]) > 0:
-#             data_files.append((key, datalist[key]))
-#     return data_files
-
-# data_files = get_datafiles()
-# data_files.append(('extras', [os.path.join('extras', f) for f in ('RaftCapture.dtd', 'RaftCaptureProcessor.py')]))
-
-# if 'darwin' == sys.platform:
-#     import py2app
-#     setup(
-#         name = 'RAFT',
-#         app = ['raft.pyw'],
-#         data_files = data_files,
-#           options = {
-#             'py2app': 
-#             {
-#                 'includes': ['lxml.etree', 'lxml._elementpath', 'gzip', 'sip'],
-#                 }
-#             }
-#         )
-#     # need to customize qt.conf to avoid dup library loading
-#     fh = open('dist/RAFT.app/Contents/Resources/qt.conf', 'wb')
-#     fh.write('')
-#     fh.close()
-
-# elif 'win32' == sys.platform:
-#     import py2exe
-
-#     find_MSVCP90()
-
-#     setup(
-#         name = 'RAFT',
-#         console = ['raft.pyw'],
-#           data_files = data_files,
-#           options = {
-#             'py2exe': 
-#             {
-#                 'includes': ['lxml.etree', 'lxml._elementpath', 'gzip', 'sip'],
-#                 }
-#             }
-#           )
-# else:
-#     pass
-
